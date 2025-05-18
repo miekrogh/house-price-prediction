bd5905b47e0cdd809779581c8278b01d60b78308
miekrogh <62981654+miekrogh@users.noreply.github.com>
2025-05-18 17:35:44 +0200

    Added /health endpoint and logging functionality

--- app/service_api.py
+++ app/service_api.py
@@ -1,4 +1,4 @@
-
+import logging, sys
 import pandas as pd
 from flask import Flask, request, jsonify
 from skops.io import load, get_untrusted_types
@@ -16,41 +16,82 @@ class InputSchema(Schema):
 
 
 def create_app():
+  # Set up logging
+  file_handler = logging.FileHandler('app.log')
+  file_handler.setLevel(logging.INFO)
+  formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+  file_handler.setFormatter(formatter)
+  
   # Initialize API
   app = Flask(__name__)
+  if not app.logger.handlers:
+    app.logger.addHandler(file_handler)
+  app.logger.setLevel(logging.INFO)
+
+  app.logger.info("Service API is running and logging is active.")
 
   # Load model at startup
   model_path = 'ml/model.skops'
   unknown_types = get_untrusted_types(file=model_path)
-  model = load(model_path, trusted=unknown_types)
+  try:
+    model = load(model_path, trusted=unknown_types)
+    app.logger.info(f"Successfully loaded model from {model_path}.")
+  except Exception as e:
+    app.logger.warning(f"Failed to load model from {model_path}: {e}")
+    sys.exit(1) # stop running if model doesn't load correctly
 
   # Attach model to app
   app.model = model
-
+  
   input_schema = InputSchema()
 
 
+  """
+  Run model inference on given input sample.
+  Returns: Predicted price if input is valid, error otherwise.
+  """
   @app.route('/predict', methods=['POST'])
   def predict():
+    logging_info = f"{request.method} {request.path}"
+
     try:
       # Get JSON input
       json_input = request.get_json()
+      app.logger.info(f"{logging_info} - Received input: {json_input}")
 
       # Validate input
       validated_input = input_schema.load(json_input)
+      app.logger.info(f"{logging_info} - Input passed validation: {validated_input}.")
 
       # Convert to dataframe
       input_df = pd.DataFrame([validated_input])
 
       # Perform inference
       predicted_price = float(app.model.predict(input_df)[0]) # native type ensures correct JSON serialization
+      app.logger.info(f"{logging_info} - Prediction result: {predicted_price}.")
 
       # Return prediction
       return jsonify({"predicted_price": predicted_price}), 200 # OK
 
     except ValidationError as err:
       # Throw error if validation fails
+      app.logger.warning(f"{logging_info} - Input failed validation: {err.messages}")
       return jsonify({"error": err.messages}), 400 # bad request
+
+
+  """
+  Get model health.
+  Returns: Static OK response.
+  """
+  @app.route('/health', methods=['GET'])
+  def health():
+    logging_info = f"{request.method} {request.path}"
+    message = "Model is healthy"
+    app.logger.info(f"{logging_info} - {message}")
+    return message, 200 # OK
+
+    
+  return app
 
 
 def main():
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,13 +14,19 @@ def client(app):
   return app.test_client()
 
 
+"""
+The model is loaded correctly when starting up the app.
+"""
 def test_model_loads_correctly(app):
   model = app.model
   assert model is not None
   assert hasattr(model, 'predict')
   check_is_fitted(model)
-  
 
+
+"""
+The /predict endpoint returns predicted price and status code 200 (OK) when given valid JSON input.
+"""
 def test_predict_returns_predicted_price_on_valid_input(client):
   valid_input = {
       "X1_transaction_date": 2013.333,
@@ -37,3 +43,30 @@ def test_predict_returns_predicted_price_on_valid_input(client):
   data = response.get_json()
   assert 'predicted_price' in data
   assert isinstance(data['predicted_price'], float)
+
+
+"""
+The /predict endpoint returns status code 400 (bad request) when given invalid input.
+"""
+def test_predict_returns_status_code_400_on_invalid_input(client):
+  invalid_input = {
+    'X1_transaction_date': 2013.333,
+    'X2_house_age': 6.3,
+    'X3_distance_to_the_nearest_MRT_station': 90.45606
+    # missing three features
+  }
+
+  response = client.post('/predict', data=json.dumps(invalid_input), content_type='application/json')
+  assert response.status_code == 400
+
+  data = response.get_json()
+  assert 'error' in data
+
+
+"""
+The /health endpoint always returns status code 200 (OK).
+"""
+def test_health_returns_status_code_200(client):
+  response = client.get('/health')
+  assert response.status_code == 200
+  assert response.get_data(as_text=True) == "Model is healthy" # decode byte response
